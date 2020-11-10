23a21ab3b0cef61ff40ab4c3be01ea73097c7b2e
Tatsiana Ivonchyk <tatsiana@makers.tech>
2020-11-10 16:45:50 +0000

    Finish ability to add and see comments

--- spec/features/user_can_submit_comments_for_post_spec.rb
+++ spec/features/user_can_submit_comments_for_post_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
-  scenario "Can submit comments and view them" do
+  scenario "can submit comments and view them" do
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
-    page.find('p', text: 'Hello, world!').click_link('Add comment')
-    fill_in "Message", with: "I am a comment"
+    click_on "Hello, world!"
+    fill_in "comment[body]", with: "I am a comment"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
     expect(page).to have_content("I am a comment")
